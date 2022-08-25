f658353b58271955d9663a46d412039fad3d9b6f
ismail.lukman <ismail.lukman@itcglobal.ng>
2022-08-25 16:03:05 +0200

    android util library

--- logutil/src/main/java/com/itcglobal/logutil/LogDebugger.kt
+++ logutil/src/main/java/com/itcglobal/logutil/LogDebugger.kt
@@ -2,7 +2,7 @@ package com.itcglobal.logutil
 
 import android.util.Log
 
-class LogDebug {
+class LogDebugger {
     val TAG = "SUPER_AWESOME_APP"
     fun d(message: String){
         Log.d(TAG, message)
